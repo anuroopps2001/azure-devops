59bc1636ab4b401811161879556b7785d0f97c9c
Anuroop P S <anuroopps2001@gmail.com>
2026-05-06 10:23:30 +0530

    updated source code to listen on port 80[skip ci]

--- 01-first-deployment/src/app.py
+++ 01-first-deployment/src/app.py
@@ -1,32 +1,37 @@
-import sys
 import os
+from flask import Flask
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
-from azure.core.exceptions import HttpResponseError
 
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    return "AKS App Running!"
+
+@app.route("/upload")
 def upload():
-    try:
-        account_name = os.environ["STORAGE_ACCOUNT"]
-        container_name = os.environ.get("CONTAINER_NAME", "appdata")
-        blob_name = "from-app.txt"
 
-        credential = DefaultAzureCredential()
-        blob_service_client = BlobServiceClient(
-            f"https://{account_name}.blob.core.windows.net",
-            credential=credential
-        )
+    account_name = os.environ["STORAGE_ACCOUNT"]
+    container_name = os.environ.get("CONTAINER_NAME", "appdata")
+    blob_name = "from-app.txt"
 
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        data = b"Hello from AKS using Workload Identity!"
+    credential = DefaultAzureCredential()
 
-        print(f"Attempting to upload to container: {container_name}...")
-        blob_client.upload_blob(data, overwrite=True)
-        print("Upload successful")
+    blob_service_client = BlobServiceClient(
+        f"https://{account_name}.blob.core.windows.net",
+        credential=credential
+    )
 
-    except Exception as e:
-        print(f"CRITICAL ERROR: {e}")
-        # This exit code 1 is what tells K8s the pod failed
-        sys.exit(1)
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name,
+        blob=blob_name
+    )
+
+    data = b"Hello from AKS using Workload Identity!"
+    blob_client.upload_blob(data, overwrite=True)
+
+    return "Upload successful"
 
 if __name__ == "__main__":
-    upload()
+    app.run(host="0.0.0.0", port=80)
